48ff64cfa74f39abad6a45fceb35187ae57491ed
rajae <bouzambourajae@gmail.com>
2024-12-05 16:09:21 +0100

    rama principal

---
+++ examenprojet/src/examen.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class examen {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int opcion;
+        string frase1,frase2,fraseconcatenada;
+        System.out.println("Menu :");
+        System.out.println("1 :Concatenar dos palabras");
+        System.out.println("2 :Visualizar el número de palabras de una frase:");
+        System.out.println("3 :Contar cuántas veces aparece una letra en una frase:");
+        System.out.println("4 : Salir");
+        System.out.println("Elige una opcion");
+        opcion=sc.nextInt();
+        switch (opcion){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            default:
+        }
+
+
+    }
+}
